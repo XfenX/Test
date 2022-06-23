ee105ac2f75f8eabe604a6183e3ac283a0235879
sader.peron@mail.ru <sader.peron@mail.ru>
2022-06-23 19:57:05 +0300

    dad

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace testikaaaaaaaaaaaaa
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 ka = new Window1();
+            this.Close();
+            ka.Show();
+        }
+
+        private void search_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (search.Text == "введите для поиска")
+            {
+                search.Text = "";
+            }
+        }
+    }
+}
---
+++ Window1.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace testikaaaaaaaaaaaaa
+{
+	/// <summary>
+	/// Логика взаимодействия для Window1.xaml
+	/// </summary>
+	public partial class Window1 : Window
+	{
+		public Window1()
+		{
+			
+			InitializeComponent();
+		}
+
+		private void Auth(object sender, RoutedEventArgs e)
+		{
+			Window k = new MainWindow();
+			
+			if (Login.Text == "Admin" && password.Password == "Admin")
+			{ 
+				this.Close();
+				k.Show();
+			}
+			else MessageBox.Show("НЕ ПАРВИЛЬНО УЁБА");
+			Login.Clear();
+			password.Clear();
+		}
+
+		private void Clear(object sender, RoutedEventArgs e)
+		{
+			Login.Text = "";
+			password.Password = "";
+		}
+
+        private void Login_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+			if (Login.Text == "Имя")
+            {
+				Login.Text = "";
+			}
+        }
+    }
+}
